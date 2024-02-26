ceb870eefede927f40f8100383b3014c30406b32
Sourav <sourav0w@gmail.com>
2024-02-26 22:58:41 +0530

    update Bookign

--- app/api/curr-ex/route.ts
+++ app/api/curr-ex/route.ts
@@ -1,48 +1,37 @@
 import axios from "axios";
 import { NextResponse, NextRequest } from "next/server";
 var currencyExchangeRate = require("currency-exchange-rate");
-const converter = require('currency-exchanger-js');
+const converter = require("currency-exchanger-js");
 export async function POST(req: Request) {
-    try {
+  try {
+    const { to } = await req.json();
+    let value;
 
-        const { to } = await req.json()
-
-
-        let value;
-
-
-
-
-
-        if (to === "AED") {
-            value = 1
-        } else {
-            value =
-                await axios.get(
-                    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/aed.json`)
-                    .then((res) => {
-                        return res.data?.aed[to.toLowerCase()]
-                    })
-        }
-
-
-
-        const response = NextResponse.json({
-            status: true,
-            exRate: value
+    if (to === "AED") {
+      value = 1;
+    } else {
+      value = await axios
+        .get(
+          `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/aed.json`
+        )
+        .then((res) => {
+          return res.data?.aed[to.toLowerCase()];
         });
-
-        // response.cookies.set('uid', id, {
-        //     maxAge: 60 * 60 * 24 * 7,
-        // });
-        return response;
-
-
-
-        // cookies().set('uid', "")
-    } catch (error) {
-        console.log("ERROR", error);
-
-        return NextResponse.json({ status: false, })
     }
+    const response = NextResponse.json({
+      status: true,
+      exRate: value,
+    });
+
+    // response.cookies.set('uid', id, {
+    //     maxAge: 60 * 60 * 24 * 7,
+    // });
+    return response;
+
+    // cookies().set('uid', "")
+  } catch (error) {
+    console.log("ERROR", error);
+
+    return NextResponse.json({ status: false });
+  }
 }
--- app/api/stripe-intent/route.ts
+++ app/api/stripe-intent/route.ts
@@ -1,36 +1,36 @@
 import { NextResponse } from "next/server";
+
 export const POST = async (req: Request) => {
-    const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET);
-    const body: any = await req.json();
+  const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET);
+  const body: any = await req.json();
+  console.log(body);
 
-    const paymentIntent = await stripe.paymentIntents.create({
-        currency: body?.currency,
-        amount: body?.amount,
-        automatic_payment_methods: {
-            enabled: true,
-        },
-        // payment_method_types: ["card", "apple_pay"],
-        description: `${body?.user?.name} (${body?.user?.phone})`,
-        shipping: {
-            name: body?.user?.name,
-            address: {
-                line1: body?.user?.address?.address,
-                postal_code: body?.user?.address?.pincode,
-                city: body?.user?.address?.city,
-                state: body?.user?.address?.state,
-                country: body?.user?.address?.country?.countryName,
-            },
-        },
-    })
+  const paymentIntent = await stripe.paymentIntents.create({
+    currency: body?.currency,
+    amount: body?.amount,
+    automatic_payment_methods: {
+      enabled: true,
+    },
 
+    description: `Pay Medx Pharmacy - (${body?.user?.phone})`,
+    shipping: {
+      name: body?.user?.address.name,
+      address: {
+        line1: body?.user?.address?.address,
+        postal_code: body?.user?.address?.pincode,
+        city: body?.user?.address?.city,
+        state: body?.user?.address?.state,
+        country: body?.user?.address?.country?.countryName,
+      },
+    },
+  });
 
-    let response = {
-        clientSecret: paymentIntent.client_secret
-    };
+  let response = {
+    clientSecret: paymentIntent.client_secret,
+  };
 
+  // const response = await fetchHomeSections();
+  console.log("Server Error =", response);
 
-    // const response = await fetchHomeSections();
-    console.log("Server Err0r = ", response);
-
-    return NextResponse.json(response);
-} 
+  return NextResponse.json(response);
+};
