32768bbe135e1f3f40733390717fe5cdeb03fc7a
Your Name <you@example.com>
2024-02-29 18:10:08 +0000

    update payment

--- app/api/stripe/error/[id]/route.ts
+++ app/api/stripe/error/[id]/route.ts
@@ -4,20 +4,31 @@ import { db } from "../../../../../config/firebase-config";
 
 
 export const GET = async (req: NextRequest, { params }) => {
+    const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET);
     const payment_intent = req.nextUrl.searchParams.get("payment_intent");
 
     const data = await getDoc(doc(db, "orders", params.id));
     const order = data.data();
+    // update Payment Status 
+    const res = await updateDoc(doc(db, "orders", params.id), {
+        "payment.status": "failed",
+        "status": "Rejected",
+        "payment.completed": false,
+        "payment.details": {
+            id: params.id,
+            paymentIntent: payment_intent,
+            message: "Payment Failed"
+        },
+    });
+    console.log(order, res);
 
-    const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET);
     try {
         const paymentIntent = await stripe.paymentIntents.update(payment_intent, {
-            description: `Medx Pharmacy - Payment Field - OrderId: ${order.orderId}`,
+            description: `Medx Pharmacy - Payment Failed - OrderId: ${order.orderId}`,
         });
-
+        await stripe.paymentIntents.cancel(payment_intent);
         return NextResponse.redirect(process.env.NEXT_PUBLIC_API_DOMAIN + `/payment-failed`);
     } catch (error) {
         return NextResponse.redirect(process.env.NEXT_PUBLIC_API_DOMAIN + `/payment-failed`);
     }
-
 }
--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -25,7 +25,6 @@ const PaymentSuccess = ({ searchParams }) => {
         currency: orderData?.currency,
       },
     });
-
   }
 
   function createButton() {
